ac050fa7789b208df600ed7232fb36b9867c5ce7
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-10-05 16:38:32 +0530

    Updated

--- src/main/java/com/atmecs/automating_travelsite/basetest/BaseTest.java
+++ src/main/java/com/atmecs/automating_travelsite/basetest/BaseTest.java
@@ -5,12 +5,16 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
 import com.atmecs.automating_travelsite.constants.Constants;
 import com.atmecs.automating_travelsite.utilities.PropertyReader;
-import com.atmecs.automating_travelsite.utilities.TestNGListeners;
+
 
 public class BaseTest 
 {
@@ -20,16 +24,31 @@ public class BaseTest
 	String browserUrl;
 	
 	@BeforeMethod
-	public void beforeTest() throws InterruptedException, IOException 
+	@Parameters("browser")
+	public void beforeTest(String browser) throws InterruptedException, IOException 
 	{
 		properties = PropertyReader.readProperties(Constants.CONFIG);
 		baseUrl = properties.getProperty("url");
 		browserUrl = properties.getProperty("browser");
 		
-		System.setProperty(Constants.USER_DIR, Constants.CHROME_PATH);
-		driver = new ChromeDriver();
-		driver.get(baseUrl);
-		
+		if(browser.equalsIgnoreCase("Chrome"))
+		{
+			System.setProperty(Constants.CHROME, Constants.CHROME_PATH);
+			driver = new ChromeDriver();
+			driver.get(baseUrl);
+		}
+		else if (browser.equalsIgnoreCase("Firefox"))
+		{
+			System.setProperty(Constants.FIREFOX, Constants.FIREFOX_PATH);
+			driver = new FirefoxDriver();
+			driver.get(baseUrl);
+		}
+		else if(browser.equalsIgnoreCase("IE"))
+		{
+			System.setProperty(Constants.IE, Constants.IE_PATH);
+			driver = new InternetExplorerDriver();
+			driver.get(baseUrl);
+		}
 		System.out.println(driver.getCurrentUrl());
 		Assert.assertEquals(driver.getCurrentUrl(),baseUrl);
 		
--- src/main/java/com/atmecs/automating_travelsite/constants/Constants.java
+++ src/main/java/com/atmecs/automating_travelsite/constants/Constants.java
@@ -2,8 +2,12 @@ package com.atmecs.automating_travelsite.constants;
 
 public class Constants 
 {
-	public static final String USER_DIR = "webdriver.chrome.driver";
+	public static final String CHROME = "webdriver.chrome.driver";
+	public static final String IE = "webdriver.ie.driver";
+	public static final String FIREFOX = "webdriver.gecko.driver";
 	public static final String CHROME_PATH = "./lib/chromedriver.exe";
+	public static final String IE_PATH = "./lib/IEDriverServer.exe";
+	public static final String FIREFOX_PATH = "./lib/geckodriver.exe";
 	public static final String DATAS = "./src/main/resources/datas/testdatas.properties";
 	public static final String CONFIG = "./src/main/resources/datas/config.properties";
 	public static final String LOCATORS = "./src/main/resources/locators/locators.properties";
